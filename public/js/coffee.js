9c3b825898d16dc37d2504cdc8f00a1521d6a32b
chagers <cehager@gmail.com>
2016-05-01 15:17:23 -0700

    Add regex for validation, refactor css, update focus in coffee script

--- public/js/coffee.js
+++ public/js/coffee.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   $(".parallax").parallax();
 
+  $("#guess-one").focus();
+
   var Entry = function(word) {
     this.word = word;
   };
@@ -13,9 +15,11 @@ $(document).ready(function () {
     }
   };
 
-  $(".btn-large").on("click", function() {
+  $("button").on("click", function() {
+
     var guessOne = new Entry($("#guess-one").val()).hasT();
     var guessTwo = new Entry($("#guess-two").val()).hasT();
+
     if (guessOne === false && guessTwo === true) {
       $("#modal1").openModal();
       $("#guess-one, #guess-two").val("");
@@ -23,6 +27,10 @@ $(document).ready(function () {
       $("#modal2").openModal();
       $("#guess-one, #guess-two").val("");
     }
+
+    $("#modal1, #modal2").on("click", function() {
+      $("#guess-one").focus();
+    });
   });
 
 });
